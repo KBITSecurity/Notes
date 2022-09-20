769a111bf3ded7749719cf4dce4470b153304b86
KBITSecurity <113555061+KBITSecurity@users.noreply.github.com>
2022-09-20 14:01:25 -0700

    Create PetlaFor.sh

---
+++ Programming/BashSH/PetlaFor.sh
@@ -0,0 +1,30 @@
+for i in {1..100};do nc 10.10.250.126 $i; echo "";done
+
+
+
+
+
+
+
+#!/bin/bash
+for dupa in {1..10}
+do
+        echo $dupa
+done
+
+
+
+
+
+
+
+
+
+
+#!/bin/bash
+list='1 3 5 23 3'
+
+for i in $list
+do
+        echo $i
+done
