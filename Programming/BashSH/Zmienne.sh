fffaf48f55ee50cd4be784b2150bca11c10f7b12
KBITSecurity <113555061+KBITSecurity@users.noreply.github.com>
2022-09-20 14:00:08 -0700

    Create Zmienne.sh

---
+++ Programming/BashSH/Zmienne.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+zmienna="siemanko" # miedzy nazwa zmiennej a wartoscia nie moze byc spacji
+
+echo $zmienna #Zmiennej uzywamy dodajac znak $ przed nazwa
+
+
+echo $zmienna
+
+echo $zmienna
+
+
+#Zmienne wpisywane w terminalu
+
+name=$1
+surname=$2
+
+echo $name $surname
+
+
+echo $#  # Wyswietli ilosc zmiennych podanych w temrinalu
+echo $0  # Wyswietli nazwe uruchomionego skryptu
