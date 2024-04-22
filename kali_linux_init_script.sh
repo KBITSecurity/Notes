f9bf014a901bd272742dc5d655960d88a0c62b7b
KBITSecurity <113555061+KBITSecurity@users.noreply.github.com>
2024-04-22 21:42:23 +0200

    Update kali_linux_init_script.sh

--- kali_linux_init_script.sh
+++ kali_linux_init_script.sh
@@ -184,7 +184,7 @@ mkdir scripts
 cd scripts
 wget https://raw.githubusercontent.com/PowerShellMafia/PowerSploit/master/Recon/PowerView.ps1
 wget https://github.com/PowerShellMafia/PowerSploit/blob/master/Recon/Invoke-Portscan.ps1
-wget -o Get-WebCredentials.ps1 https://raw.githubusercontent.com/KBITSecurity/Notes/main/creds.ps1
+wget -O Get-WebCredentials.ps1 https://raw.githubusercontent.com/KBITSecurity/Notes/main/creds.ps1
 wget https://raw.githubusercontent.com/PowerShellMafia/PowerSploit/master/Privesc/PowerUp.ps1
 ##############################################################################################
 
