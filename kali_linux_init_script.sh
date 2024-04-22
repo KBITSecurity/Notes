bba3c6eff00196c32f7e9f5018972977146d8ea2
KBITSecurity <113555061+KBITSecurity@users.noreply.github.com>
2024-04-22 17:39:07 +0200

    Update kali_linux_init_script.sh

--- kali_linux_init_script.sh
+++ kali_linux_init_script.sh
@@ -87,7 +87,7 @@ alias thm="nohup openvpn /home/kali/Binio.ovpn &"
 export ALIEN_THEME="red"
 source /opt/software/alien/alien.zsh
 export ext="pdf,xls,tar,gz,rar,zip,doc,txt,yaml,yml,xls,bak,back,php,htm,html,asp,aspx,jsp,json,docx"
-wmctrl -r ":ACTIVE:" -b toggle,fullscreen
+#wmctrl -r ":ACTIVE:" -b toggle,fullscreen
 alias cb="xclip -select clipboard"
 alias download="mv /home/kali/Downloads/* ."
 ' >> /root/.zshrc
