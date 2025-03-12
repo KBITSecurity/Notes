0b5087a0ccfef461f8f6bd529395d3ed22eeb035
KBITSecurity <113555061+KBITSecurity@users.noreply.github.com>
2025-03-12 23:36:40 +0100

    Update kali_windows_init_script.ps1

--- kali_windows_init_script.ps1
+++ kali_windows_init_script.ps1
@@ -9,3 +9,24 @@ Get-ChildItem -Path "C:\tools\wordlists\SecLists\Fuzzing\*" -Filter "*.txt" -Rec
 Get-Content first,second | Set-Content together
 Get-Content together | Sort-Object -Unique | Set-Content webFullListEndpointsPayloads
 del first,second,together
+
+
+
+
+'''
+function sqlmap {
+        python C:\tools\sqlmap\sqlmap.py $args
+}
+function gobuster {
+        C:\tools\Enumeracja\gobuster.exe $args
+}
+function feroxbuster {
+        C:\tools\Enumeracja\feroxbuster.exe $args
+}
+function ffuf {
+        C:\tools\Enumeracja\ffuf.exe $args
+}
+function nc {
+        C:\tools\nc\nc64.exe $args
+}
+''' > C:\Users\Krzys\Documents\WindowsPowerShell\Microsoft.PowerShell_profile.ps1
