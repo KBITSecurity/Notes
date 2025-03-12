56d8ca62653b50bc24ae9b77cd5cfd56b4e2e436
KBITSecurity <113555061+KBITSecurity@users.noreply.github.com>
2025-03-12 23:29:51 +0100

    Update kali_windows_init_script.ps1

--- kali_windows_init_script.ps1
+++ kali_windows_init_script.ps1
@@ -4,3 +4,8 @@ cd C:\tools\wordlists
 
 git clone https://github.com/danielmiessler/SecLists.git
 
+Get-ChildItem -Path "C:\tools\wordlists\SecLists\Discovery\*" -Filter "*.txt" -Recurse | Get-Content > first
+Get-ChildItem -Path "C:\tools\wordlists\SecLists\Fuzzing\*" -Filter "*.txt" -Recurse | Get-Content > second
+Get-Content first,second | Set-Content together
+Get-Content together | Sort-Object -Unique | Set-Content webFullListEndpointsPayloads
+del first,second,together
